850425c7c2bee67439818f679017f1fef8394804
Paula Beatriz <107193691+Paulabeatriz09@users.noreply.github.com>
2022-09-29 19:01:04 -0300

    primeiro commit

---
+++ exerciciospython.py
@@ -0,0 +1,76 @@
+#numeros=[]
+# tamanho=3
+#
+# for i in range (tamanho):
+#     numeros.append(int(input("Digite 20 números:")))
+#
+# for i in range (3,0,-1):
+#     print(numeros)
+
+# Faça um código para ler 5 nomes de usuários e sua respectivas senhas
+# e armazenar cada lista em um array diferente, após completar a digitação,
+# imprimir o nome, senha e posição no array
+
+# usuario=[]
+# senha=[]
+# tamanho=int(input("Digite quantos usuarios você deseja inserir: "))
+#
+# for i in range (tamanho):
+#     usuario.append(input("Digite o nome do usuário:"))
+#
+# for i in range (tamanho):
+#     senha.append(input("Digite a senha do usuário:"))
+#
+# for i in range (tamanho):
+#     print(i,usuario[i],senha[i])
+    # print(i,end=" ")
+    # print(usuario[i], end=" ")
+    # print(senha[i])
+
+# Altere o sistema anterior e faça um sistema de login, pedindo a senha do usuario
+# e mostrando seu nome e a mesnagem, login efetuado com sucesso
+# usuario=[]
+# senha=[]
+# tamanho=int(input("Digite quantos usuarios você deseja inserir: "))
+#
+# for i in range (tamanho):
+#     usuario.append(input("Digite o nome do usuário:"))
+#
+# for i in range (tamanho):
+#     senha.append(input("Digite a senha do usuário:"))
+#
+# for i in range (tamanho):
+#     senha1=input("Digite novamente sua senha: ")
+#     if(senha1 in senha):
+#         print("Login concluído")
+#     else:
+#         print("Login bloqueado")
+# faça um código para ler um vetor de 30 números. Após isto, ler mais um número qualquer, calcular e escrever quantas
+# esse numero aparece no vetor.
+# numeros=[]
+# cont=0
+# tamanho=int(input("Digite quantos números você deseja inserir no vetor: "))
+#
+# for i in range (tamanho):
+#     numeros.append(input("Digite os valores:"))
+#
+# numero1=input("digite um valor: ")
+#
+# if(numero1 in numeros):
+#  cont+=1
+#
+# print("O número: ",numero1," apareceu: ",cont," vezes no vetor")
+
+# escreva um algoritmo que solicite ao usuario a entrada de 5 nomes e que exiba a lista desses nomes na tela
+#após exibir essa lista, o programa deve mostrar tambéem os nomes na ordem inversa em que o usuario digitou um por linha
+# nomes=[]
+# tamanho=int(input("Digite quantos elementos você deseja inserir no vetor: "))
+
+# for i in range (tamanho):
+#     nomes.append(input("Digite os nomes:"))
+
+# print(nomes)
+
+# for i in range (tamanho,0,-1):
+#  print(nomes)
+
