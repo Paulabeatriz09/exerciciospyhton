99e6196da2e08e956e006ebdeac48d59fe99c3a6
Paula Beatriz <107193691+Paulabeatriz09@users.noreply.github.com>
2022-09-29 19:07:00 -0300

    segundo commit

---
+++ python.py
@@ -0,0 +1,86 @@
+# numero=int(input("Digite um número: "))
+#
+# if(numero>0):
+#     print("Número positivo")
+#
+# elif(numero<0):
+#     print("Número negativo")
+#
+
+# numero = int(input("Digite um número: "))
+#
+# while(numero==0):
+#     numero = int(input("Digite um número: "))
+#
+# if(numero>0):
+#     print("Número positivo")
+# elif(numero<0):
+#     print("Número negativo")
+
+# idade=int(input("Digite sua idade: "))
+#
+# ano=2022-idade
+#
+# print("Você nasceu em: ",ano)
+
+# n1=int(input("Digite um número: "))
+# n2=int(input("Digite um número: "))
+# n3=int(input("Digite um número: "))
+#
+# if(n1>n2 and n1>n3):
+#     print("O número ",n1," é maior que ",n2,n3)
+# elif(n2>n1 and n2>n3):
+#     print("O número ",n2," é maior que ",n1,n3)
+# else:
+#     print("O ",n3," é maior que: ",n1, n2)
+
+# numero=int(input("Digite um número: "))
+#
+# ant=numero-1
+#
+# print("O antecessor é: ",ant)
+
+# anos=int(input("Quantos anos você tem:"))
+# meses=int(input("Quantos meses: "))
+#
+# dias=(anos*365)+(meses*30)
+#
+# print("Você tem: ",dias," dias de vida")
+
+# enc='não'
+#
+# while (enc=='não'):
+#
+#     n1=int(input("Digite um número: "))
+#     n2=int(input("Digite outro número: "))
+#
+#     if(n2>n1):
+#         print(n1,n2)
+#     else:
+#         print(n2,n1)
+#
+#     enc = str(input("Caso deseje finalizar escreva -sim-, ou -não- para continuar?"))
+#     if (enc == 'sim'):
+#         break
+
+# Escreva um algoritmo para ler a hora de inicio e a hora de fim de um jogo de Xadrez, e calcule a duraçãodo jogo em horas
+# h1=int(input("Digite a hora inicial do jogo: "))
+# h2=int(input("Digite a hora final do jogo: "))
+#
+# if(h2>h1):
+#     dif=(h2-h1)
+#     print("O jogo durou:",dif," horas")
+# else:
+#     de=(24-h1)+h2
+#     print("O jogo durou: ",de," horas")
+# As maças custam R$1,30 cada se forem compradas menos de uma dúzia, e R$1,30
+
+# maca=int(input("Quantas maças você deseja comprar ? "))
+# 
+# if(maca>=12):
+#     preco=1*maca
+#     print("O valor deu: R$ ".preco)
+# 
+# else:
+#     preco1=1.3*maca
+#     print("O valor deu: R$ ",preco1)
